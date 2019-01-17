1ba34b1dd9abac3d71cabc64430d2a4224a364d2
David Gonzalez Jr <davidgonzalezjr@yahoo.com>
2019-01-17 13:58:46 -0500

    Begining the playermovement code to the player

---
+++ New Unity Project/Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rB;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    rB.AddForce(0, 200, 500);//Adds force to the x, y, z axis of the player
+    //}
+
+    // Update is called once per frame
+    //FixUpdate is used when you are messing with the physics on Unity
+    void FixUpdate()
+    {
+        //Time.deltaTime fixes the frame rates on the computer
+        rB.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
