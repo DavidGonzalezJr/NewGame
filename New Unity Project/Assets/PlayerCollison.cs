5e2dbf3d25483cf4249f3f2c8ec114d2f4460d38
David Gonzalez Jr <davidgonzalezjr@yahoo.com>
2019-01-17 15:38:28 -0500

    Added an obstcale and collison functions

---
+++ New Unity Project/Assets/PlayerCollison.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollison : MonoBehaviour
+{
+
+    public PlayerMovement movement;
+
+    //Unity has special methods for collisons and how to make them work in the game
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstcale")
+        {
+            movement.enabled = false;
+        }
+
+    }
+
+
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+}
